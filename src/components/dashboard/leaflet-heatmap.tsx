7fbd2dab0a59ade7ae533277c5128c37e4b53ec2
Dulani Woods <dulani@gmail.com>
2025-10-06 10:44:30 +0000

    Try fixing this error: `Runtime Error: Error: Map container is already i

--- src/components/dashboard/leaflet-heatmap.tsx
+++ src/components/dashboard/leaflet-heatmap.tsx
@@ -33,7 +33,7 @@ export function LeafletHeatmap({ signalData }: LeafletHeatmapProps) {
   // key={center.toString()} is a bit of a hack, but it forces a re-render when the center changes
   // which is what we want when the data changes. This is a common pattern for react-leaflet.
   return (
-    <MapContainer key={center.toString()} center={center} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
+    <MapContainer key={JSON.stringify(center)} center={center} zoom={13} scrollWheelZoom={true} style={{ height: '100%', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
